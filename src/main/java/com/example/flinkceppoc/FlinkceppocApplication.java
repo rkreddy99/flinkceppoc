33be0ad3bba09763781179dad14511358c182687
rama reddy <rama.reddy@gravitontrading.com>
2025-03-15 12:04:05 +0530

    initial commit

---
+++ src/main/java/com/example/flinkceppoc/FlinkceppocApplication.java
@@ -0,0 +1,13 @@
+package com.example.flinkceppoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkceppocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlinkceppocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flinkceppoc/FlinkceppocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flinkceppoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkceppocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
